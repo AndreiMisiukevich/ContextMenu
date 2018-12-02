0a9914ca34a2913ee646433fa04b6ceffc01c0c0
Andrei Misiukevich <andrei.misiukevich@gmail.com>
2018-12-02 23:46:27 +0300

    small refactoring

--- ContextMenu/MoveToActionCell.cs
+++ ContextMenu/MoveToActionCell.cs
@@ -9,6 +9,7 @@ namespace ContextMenu
         private View _prevContext;
 
         public static readonly BindableProperty MovedCommandProperty = BindableProperty.Create(nameof(MovedCommand), typeof(ICommand), typeof(MoveToActionCell), null);
+
         public static readonly BindableProperty IsAutoCloseEnabledProperty = BindableProperty.Create(nameof(IsAutoCloseEnabled), typeof(bool), typeof(MoveToActionCell), true);
 
         public event Action<object> Moved;
@@ -24,7 +25,9 @@ namespace ContextMenu
                 Device.BeginInvokeOnMainThread(() =>
                 {
                     if (IsAutoCloseEnabled)
+                    {
                         ForceClose();
+                    }
                     Moved?.Invoke(BindingContext);
                     MovedCommand?.Execute(BindingContext);
                 });
