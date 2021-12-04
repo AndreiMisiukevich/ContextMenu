89cf2168ebeeff8f039193a9fdc270fa27295121
Kirill Lyubimov <dev.lyubimov.kirill@gmail.com>
2021-12-05 01:32:20 +0400

    Samples: Add samples for ListView

---
+++ ContextMenuSample/ContextMenuSample/Pages/ScrollViewInsideListViewPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ContextMenuSample.Pages
+{
+    public partial class ScrollViewInsideListViewPage : ContentPage
+    {
+        public ScrollViewInsideListViewPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ContextMenuSample/ContextMenuSample/ViewModels/ScrollViewInsideListViewViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ContextMenuSample.ViewModels
+{
+    public class ScrollViewInsideListViewViewModel
+    {
+        public List<List<int>> Items { get; }
+
+        public ScrollViewInsideListViewViewModel()
+        {
+            Items = new List<List<int>>();
+
+            for (int i = 0; i < 500; i++)
+            {
+                var internalItems = new List<int>();
+
+                for (int j = 0; j < 9; j++)
+                {
+                    internalItems.Add(j + 1);
+                }
+
+                Items.Add(internalItems);
+            }
+        }
+    }
+}
